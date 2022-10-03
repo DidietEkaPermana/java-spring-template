a0fbe20a6a38b9e6c384336126009acdca18dc11
Didiet Eka Permana <didietekapermana@Didiets-MacBook-Air.local>
2022-10-03 20:19:41 +0700

    with mapped superclass to extend jpa class

--- src/main/java/com/example/demo/entity/AuditData.java
+++ src/main/java/com/example/demo/entity/AuditData.java
@@ -2,6 +2,9 @@ package com.example.demo.entity;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +15,17 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
+@MappedSuperclass
 public class AuditData {
+    @Column(name = "createdDate", nullable = false)
     private Date CreatedDate;
+
+    @Column(name = "createdBy", nullable = false)
     private String CreatedBy;
+
+    @Column(name = "lastUpdateDate", nullable = false)
     private Date LastUpdateDate;
+
+    @Column(name = "lastUpdateBy", nullable = false)
     private String LastUpdateBy;
 }
